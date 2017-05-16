3ddd4ac0519367ffefdca5d0501d6e566675f09f
fabian <fabian121@gmail.com>
2017-05-15 23:44:59 -0600

    My First commit

---
+++ mainwindow.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <string>
+
+using namespace std;
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_btnSayHello_clicked()
+{
+    string sname = "";
+    QString name = ui->txtName->toPlainText();
+
+    ui->txtResult->setPlainText("Hello World " + name + "!");
+}
